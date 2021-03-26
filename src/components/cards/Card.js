949792d6e753d69a97662fd26a2a1efd603c6f08
Farrukhkhalid <farrukhkhalid.it@outlook.com>
2021-03-25 17:30:36 -0700

    update lastupdate of 26 march

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import { Slide ,Card, CardContent, Typography, Grid } from "@material-ui/core";
 import Countup from "react-countup";
 import cx from 'classnames';
 
@@ -8,33 +8,35 @@ function Cards(props) {
   return ( 
     <div className='CardContainer'>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card} className='CardGrid infected'>
-          <CardContent className='Card'>
-            <Typography color="textSecoundary">Infected</Typography>
+         <Grid item component={Card} className='CardGrid infected'>
+          <CardContent className='Card' >
+            <Typography>INFECTED</Typography>
             <Typography variant="h5">
-              <Countup
+            <Countup
                 start={0}
                 end={props.data.recovered}
-                duration={1.5}
+                duration={2}
                 separator=","
               />
             </Typography>
-            <Typography>Real Data</Typography>
+            <Typography>{new Date(props.data.lastUpdate).toDateString()}</Typography>
             <Typography variant="body2">
               number of active cases in covid
             </Typography>
           </CardContent>
-        </Grid>
+       </Grid>
+ 
         <Grid item component={Card} className='CardGrid recovered'>
         <CardContent className='Card'>
-            <Typography color="textSecoundary">Recovered</Typography>
-            <Typography variant="h5"><Countup
+            <Typography color="textSecoundary">RECOVERED</Typography>
+            <Typography variant="h5">
+                <Countup
                 start={0}
                 end={props.data.deaths}
-                duration={1.5}
+                duration={2}
                 separator=","
               /></Typography>
-            <Typography>Real Data</Typography>
+            <Typography>{new Date(props.data.lastUpdate).toDateString()}</Typography>
             <Typography variant="body2">
               number of active cases in covid
             </Typography>
@@ -42,14 +44,14 @@ function Cards(props) {
         </Grid>
         <Grid item component={Card} className='CardGrid deaths'>
         <CardContent className='Card'>
-            <Typography color="textSecoundary">Death</Typography>
+            <Typography color="textSecoundary">DEATHS</Typography>
             <Typography variant="h5"><Countup
                 start={0}
                 end={props.data.confirmed}
-                duration={1.5}
+                duration={2}
                 separator=","
               /></Typography>
-            <Typography>Real Data</Typography>
+            <Typography>{new Date(props.data.lastUpdate).toDateString()}</Typography>
             <Typography variant="body2">
               number of active cases in covid
             </Typography>
